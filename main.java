c8a418feb46ca849b8d47213bcce402c54cf3143
solomongo™ <80227002+solomongoGUI@users.noreply.github.com>
2022-09-26 01:13:06 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+/**
+ * @author DAW120
+   📌 EJERCICIO 6, Hoja 2.   
+ 
+   🔴 Programa que declare una variable para almacenar el radio de un cilindro y 
+     otra que permita almacenar su altura. 
+      
+   🔴 Asigna a ambas variables un valor y visualiza luego:
+   
+          a. Su área lateral es igual Al = 2* PI* r* h
+          b. Su volumen es igual V= PI * r^2 * h
+          
+   🔴 Utiliza la clase scanner y Math y los valores pueden contener decimales.
+ */
+package com.solomongo.ejercicio6_hoja2;
+public class Ejercicio6_Hoja2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
